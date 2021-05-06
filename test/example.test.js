c21762d97521e0491387b0e8a03c3e2cb8bd86e5
Peter Montano <pete225848@gmail.com>
2021-05-06 13:17:05 -0700

    Completed! 1.0

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,44 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { rockPaperScissors, getComputerThrow } from '../utils.js';
 
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expectWin = 'user';
+    const expectLoss = 'computer';
+    const expectDraw = 'draw';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const win = rockPaperScissors('paper', 'rock');
+    const loss = rockPaperScissors('paper', 'scissor');
+    const draw = rockPaperScissors('scissor', 'scissor');
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(expectWin, win);
+    expect.equal(expectLoss, loss);
+    expect.equal(expectDraw, draw);
+});
+    // The function below uses math.random, so I re-ran the test until each expectation passed to ensure the function is working.
+test('time to test a function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expectRock = 'rock';
+    const expectPaper = 'paper';
+    const expectScissors = 'scissors';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const rock = getComputerThrow(1);
+    const paper = getComputerThrow(2);
+    const scissors = getComputerThrow(3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(expectRock, rock);
+    expect.equal(expectPaper, paper);
+    expect.equal(expectScissors, scissors);
 });
